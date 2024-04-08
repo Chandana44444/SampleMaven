be80e79b16239b72d33a4172e73834f936d60fed
Chandana M <Chandana M@TYSS-Chandana-M>
2024-04-08 17:58:44 +0530

    1st push

---
+++ Sample_maven/src/test/java/package_02/Launch_03_Test.java
@@ -0,0 +1,16 @@
+package package_02;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Launch_03_Test {
+	@Test
+	public void launch() throws InterruptedException {
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("https://www.kalkifashion.com/");
+	Reporter.log("package2,script-01",true);
+	Thread.sleep(2000);
+	driver.quit();
+}
+}
---
+++ Sample_maven/src/test/java/package_02/Launch_04_Test.java
@@ -0,0 +1,17 @@
+package package_02;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Launch_04_Test {
+	@Test
+	public void launch() throws InterruptedException {
+	String url = System.getProperty("url");
+	ChromeDriver driver=new ChromeDriver();
+	driver.get(url);
+	Thread.sleep(2000);
+	Reporter.log("package2,script-02",true);
+	driver.quit();
+}
+}
